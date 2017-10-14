a3c8d1d9ccf95eb48b2d8b2e9e0c9962e75bfe40
Jay Honnold <jayhonnold@gmail.com>
2017-10-14 00:02:35 -0400

    Added in basic styling

--- blog/blog.py
+++ blog/blog.py
@@ -13,10 +13,12 @@ app.config.update(dict(
 ))
 app.config.from_envvar('SAM_BLOG_SETTINGS', silent=True)
 
+
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
 
 def init_db():
     db = get_db()
@@ -24,17 +26,41 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
+
+def seed_db():
+    db = get_db()
+    with app.open_resource('seeds.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
 @app.cli.command('initdb')
 def initdb_command():
     init_db()
     print('Initialized the database.')
+
+
+@app.cli.command('seeddb')
+def seeddb_command():
+    seed_db()
+    print('Seeded the database.')
+
 
 def get_db():
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+
+@app.route('/')
+def index():
+    db = get_db()
+    cur = db.execute('select title, content from posts order by id desc')
+    posts = cur.fetchall()
+    return render_template('posts.html', posts=posts)
