44f37003e21602c746f4f88657787d845a1289cd
Jay Honnold <jayhonnold@gmail.com>
2018-01-04 08:45:32 -0600

    Added back in create routes

--- blog/blog.py
+++ blog/blog.py
@@ -12,6 +12,21 @@ def index():
     return render_template('index.html', posts=posts)
 
 
+@app.route('/create')
+def create():
+    return render_template('create.html', date=str(datetime.date(datetime.now())))
+
+@app.route('/create', methods=['POST'])
+def create_entry():
+    db = get_db()
+    db.execute(
+        'insert into posts (title, content, created_at, updated_at) \
+        values (?, ?, date(\'now\'), date(\'now\'))',
+        [request.form['title'], request.form['content']])
+    db.commit()
+    return redirect(url_for('index'))
+
+
 @app.template_filter('strftime')
 def _jinja2_filter_datetime(date):
     parsed = datetime.strptime(date, '%Y-%m-%d')
